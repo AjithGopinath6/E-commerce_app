f3403446ed404dc30b71ef8c040402387ed34f01
Ajith Gopinath <ajithgopinath639@gmail.com>
2024-10-16 13:28:28 +0530

    added latest collections to the website.

--- src/Components/LatestCollections.jsx
+++ src/Components/LatestCollections.jsx
@@ -1,13 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../Context/ShopContext'
+import Title from './Title';
+import ProductItems from './ProductItems';
 
 function LatestCollections() {
 
     const {products} =useContext(ShopContext);
-    console.log(products)
+    const [latestProduct, setLatestProduct] = useState([]);
+
+    useEffect(()=>{
+        setLatestProduct(products.slice(0,10));
+    },[])
+    
 
   return (
-    <div>LatestCollections</div>
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'Latest'} text2={' Collections'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi iste voluptate dolore, adipisci eos similique facere quas. Fuga minima sapiente iste aspernatur doloribus provident pariatur recusandae! Vel provident voluptate iusto.</p>
+        </div>
+
+        {/* Rendering products */}
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+                latestProduct.map((item, index)=>(
+                    <ProductItems key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+                )
+                )
+            }
+        </div>
+
+    </div>
   )
 }
 
