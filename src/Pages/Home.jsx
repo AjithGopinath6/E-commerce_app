26f7b039ac926cfe05fdce61848e927581e23da8
Ajith Gopinath <ajithgopinath639@gmail.com>
2024-10-16 15:57:28 +0530

    Added Policy section.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero'
 import LatestCollections from '../Components/LatestCollections'
 import BestSeller from '../Components/BestSeller'
+import OurPolicy from '../Components/OurPolicy'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Hero/>
       <LatestCollections/>
       <BestSeller/>
+      <OurPolicy/>
     </div>
   )
 }
