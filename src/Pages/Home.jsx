1df3b23163169df001aa65d48d86b145e1880bf4
Ajith Gopinath <ajithgopinath639@gmail.com>
2024-10-16 15:43:03 +0530

    Bestseller section added.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import LatestCollections from '../Components/LatestCollections'
+import BestSeller from '../Components/BestSeller'
 
 function Home() {
   return (
     <div>
       <Hero/>
       <LatestCollections/>
+      <BestSeller/>
     </div>
   )
 }
