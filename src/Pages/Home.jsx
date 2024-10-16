ebe766401927e4cc5afe10564919a23a10be04be
Ajith Gopinath <ajithgopinath639@gmail.com>
2024-10-16 12:38:46 +0530

    created hero section and move to latest collections.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../Components/Hero'
+import LatestCollections from '../Components/LatestCollections'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+      <LatestCollections/>
+    </div>
   )
 }
 
